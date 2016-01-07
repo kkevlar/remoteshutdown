6a76f330b65355b358295c7bba5b58b4573e52a7
Kevin Kellar <kellar.kevin@gmail.com>
2016-01-07 14:19:25 -0800

    Updated readme and PasswordCreator

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # remoteshutdown
 The simplest remote shutdown around!
+
+##Dependencies
+* [KevinLibrary] (https://github.com/flipturnapps/FlipTurnApps-KevinLibrary)
+* [jaspt] (http://www.jasypt.org/)
--- src/com/flipturnapps/remoteshutdown/common/PasswordCreator.java
+++ src/com/flipturnapps/remoteshutdown/common/PasswordCreator.java
@@ -4,13 +4,18 @@ import java.util.Calendar;
 import org.jasypt.util.password.BasicPasswordEncryptor;
 public class PasswordCreator 
 {
+	private BasicPasswordEncryptor encryptor;
+	private static final String SALT= "zSALTz";;
 	public PasswordCreator() 
 	{
-		int day = Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
+		encryptor = new BasicPasswordEncryptor();
+	}
+	public String createPassword(String password , int dayOffset)
+	{
+		int day = Calendar.getInstance().get(Calendar.DAY_OF_YEAR)+dayOffset;
 		int yr = Calendar.getInstance().get(Calendar.YEAR);
 		int daycode = day*yr;
-		BasicPasswordEncryptor encryptor = new BasicPasswordEncryptor();
-		encryptor.encryptPassword();
+		return encryptor.encryptPassword(SALT + daycode + SALT + password + SALT);
 	}
 
 }
